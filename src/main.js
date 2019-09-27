0b1ceca4960c35bb8558986fb940fa54c5f2293a
jason <1074673218@qq.com>
2019-09-27 22:14:47 +0800

    loader

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 import _ from 'lodash';
+import './css/index.css';
+import './css/index.scss';
+import bg from './assets/image/laobi.png';
+import DataXml from './mock/data.xml';
+import DataJson from './mock/data.json';
+
 function component() {
     var element = document.createElement('div');
 
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');//css
+    element.classList.add('fontsize');//sass
 
+    // 将图像添加到我们现有的 div。
+    var myIcon = new Image();
+    myIcon.src = bg;
+    // element.appendChild(myIcon);
+
+    console.log(DataXml);
+    console.log(DataJson);
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,42 @@ module.exports = {
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test:/\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test:/\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test:/\.(png|svg|jpg|gif|woff|woff2|eto|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test:/\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+      },
+      {
+        test:/\.xml$/,
+        use: [
+          'xml-loader'
+        ]
+      },
+    ]
   }
 };
