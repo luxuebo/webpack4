b5ef7e2155df9f50d29450628112e4dd5afc6f63
jason <jason_lxb@163.com>
2019-09-29 17:54:13 +0800

    htmlplug

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
     1.初始化项目：npm init
     2.npm install webpack webpack-cli --save-dev
 ## 加载css
-  1.安装css loader：npm install css-loader style-loader --save-dev  
+  1.安装css loader  
+  ```
+  npm install css-loader style-loader --save-dev  
+  ```
   2.配置rules：
   ```
   {
@@ -15,7 +18,10 @@
   }
   ```
 ## 使用sass
-1.安装sass loader：npm install node-sass sass-loader --save-dev  
+1.安装sass loader  
+```
+npm install node-sass sass-loader --save-dev
+```  
 2.配置rules:
 ```
  {  
@@ -28,7 +34,19 @@
 }
 ```
 ## 加载图片，字体
-file-loader 和 url-loader 可以接收并加载任何文件(html-loader)  
-1.npm install --save-dev file-loader url-loader  
+1.file-loader 和 url-loader 可以接收并加载任何文件(html-loader)  
+```  
+npm install --save-dev file-loader url-loader 
+```
+2.在html中引入文件(图片) 
+```
+npm install html-withimg-loader --save-dev
+```
 ## 加载数据（ CSV、TSV 和 XML，json是默认）
-1.npm install --save-dev csv-loader xml-loader  
+```
+npm install --save-dev csv-loader xml-loader  
+```
+## 打包html 安装插件
+```
+npm install --save-dev html-webpack-plugin
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,55 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
+  performance: {
+    hints: false
+  },
   entry: './src/main.js',
   output: {
-    filename: 'index.js',
+    filename: 'index[hash].js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '首页',
+      template: 'src/views/index.html'
+    })
+  ],
   module: {
     rules: [
       {
-        test:/\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test:/\.scss$/,
+        test: /\.(css|scss)$/,
         use: [
           'style-loader',
           'css-loader',
           'sass-loader'
         ]
       },
+      
       {
-        test:/\.(png|svg|jpg|gif|woff|woff2|eto|ttf|otf)$/,
+        test: /\.(png|svg|jpg|gif|woff|woff2|eto|ttf|otf)$/,
         use: [
           'file-loader'
         ]
       },
       {
-        test:/\.(csv|tsv)$/,
+        test: /\.(csv|tsv)$/,
         use: [
           'csv-loader'
         ]
       },
       {
-        test:/\.xml$/,
+        test: /\.xml$/,
         use: [
           'xml-loader'
         ]
       },
+      {
+        test: /\.(htm|html)$/i,
+        use: 'html-withimg-loader'
+      }
     ]
   }
 };
